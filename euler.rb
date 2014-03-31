c44d71373be65ac91736befd80aef867e782d223
Sarah Ransohoff <sranso@gmail.com>
2014-03-31 18:15:33 -0400

    Solve square of sums minus sum of squares

--- euler.rb
+++ euler.rb
@@ -74,3 +74,32 @@ end
 
 # puts smallest_positive
 
+# ====================
+# The sum of the squares of the first ten natural numbers is,
+# 12 + 22 + ... + 102 = 385
+# The square of the sum of the first ten natural numbers is,
+# (1 + 2 + ... + 10)2 = 552 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+def sum_of_squares
+  nums =* (1..100)
+  sum = 0
+  nums.each do |num|
+    sum += num*num
+  end
+  return sum
+end
+
+def square_of_sums
+  nums =* (1..100)
+  sum = nums.inject { |sum, num| sum + num }
+  return sum * sum
+end
+
+def difference
+  square_of_sums - sum_of_squares
+end
+# puts difference
+
+# ====================
+
