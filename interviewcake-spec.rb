6e539f8c1fcd1459e27fcc8921780eacd174af36
Sarah Ransohoff <sranso@gmail.com>
2014-04-08 17:31:46 -0400

    Solve get largest from stack

--- interviewcake-spec.rb
+++ interviewcake-spec.rb
@@ -10,3 +10,24 @@ describe 'Array' do
     end
   end
 end
+
+describe 'Stack' do
+  describe '#get_largest' do
+    stack = Stack.new(0)
+    stack.add(3)
+    stack.add(2)
+    stack.add(4)
+    stack.add(1)
+    it 'should return the largest element in the stack' do
+      expect(stack.get_largest).to eq 4
+    end
+    it 'should return the largest element in the stack even when something is removed' do
+      stack.remove
+      stack.remove
+      expect(stack.get_largest).to eq 3
+    end
+  end
+end
+
+
+
--- interviewcake.rb
+++ interviewcake.rb
@@ -1,3 +1,4 @@
+require 'debugger'
 class Array
 
   def my_reverse
@@ -8,3 +9,41 @@ class Array
   end
 
 end
+
+class Stack
+  attr_reader :history, :largest
+
+  def initialize(num)
+    @history = [num]
+    @largest = [num]
+  end
+
+  def add(num)
+    history << num
+    set_largest
+  end
+
+  def remove
+    removed = history.pop
+    if removed == largest.last
+      largest.pop
+    end
+  end
+
+  def top
+    history.last
+  end
+
+  def set_largest
+    if top >= largest.last
+      largest << top
+    end
+  end
+
+  def get_largest
+    largest.last
+  end
+
+end
+
+
