01a48d206aa25798ff8c4fd7e3c501a0db2be8c5
Sarah Ransohoff <sranso@gmail.com>
2014-04-08 16:00:07 -0400

    Not done yet but working on letters in numbers

--- euler.rb
+++ euler.rb
@@ -413,6 +413,26 @@ end
 # puts sum_power
 
 # ====================
+# If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+# If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+
+# NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+
+def letters_in_numbers
+  one_to_nine = 3 + 3 + 5 + 4 + 4 + 3 + 5 + 5 + 4
+  ten_to_nineteen = 3 + 6 + 6 + 8 + 8 + 7 + 7 + 9 + 8 + 8
+  twenty_to_ninetynine = (6 + 6 + 5 + 5 + 5 + 7 + 6 + 6) * 10 + one_to_nine * 8
+  under_onehundred = one_to_nine + ten_to_nineteen + twenty_to_ninetynine
+  hundredand = 10 * 9 * 99
+  onehundred_to_nineninetynine = one_to_nine * 99 + hundredand + ten_to_nineteen * 9 + twenty_to_ninetynine * 9
+  thousand = 3 + 8
+  return one_to_nine + ten_to_nineteen + twenty_to_ninetynine + hundredand + onehundred_to_nineninetynine + thousand
+end
+
+puts letters_in_numbers
+
+
 
 
 
