c1f6948bd5d94084a5f9d9d1e668d3de833b3a57
Sarah Ransohoff <sranso@gmail.com>
2014-04-08 10:05:54 -0400

    Solve large sum

--- euler.rb
+++ euler.rb
@@ -319,6 +319,38 @@ def find_500_divisors
   return triangle_num
 end
 require 'time'
-puts Time.now
-puts find_500_divisors
-puts Time.now
+# puts Time.now
+# puts find_500_divisors
+# puts Time.now
+
+# ====================
+# Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+def first_ten_digits
+  sum = 0
+  File.open('largesum.txt') do |file|
+    file.each_line do |line|
+      sum += line.to_i
+    end
+  end
+  return sum.to_s[0..9].to_i
+end
+
+# puts first_ten_digits
+
+# ====================
+# The following iterative sequence is defined for the set of positive integers:
+
+# n → n/2 (n is even)
+# n → 3n + 1 (n is odd)
+
+# Using the rule above and starting with 13, we generate the following sequence:
+
+# 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+# It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+
+# Which starting number, under one million, produces the longest chain?
+
+# NOTE: Once the chain starts the terms are allowed to go above one million.
+
+
+
