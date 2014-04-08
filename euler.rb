6eab64727048d414a57e232a03da55756c574d6c
Sarah Ransohoff <sranso@gmail.com>
2014-04-08 10:31:44 -0400

    Solve Longest chain

--- euler.rb
+++ euler.rb
@@ -352,5 +352,30 @@ end
 
 # NOTE: Once the chain starts the terms are allowed to go above one million.
 
+def collatz
+  longest_chain = 1
+  longest_starting = 1
+  starting_num = 1
+  while starting_num < 1000000
+    n = starting_num
+    chain = 1 # accounting for 1, the end of every chain
+    while n != 1
+      if n.even?
+        n = n/2
+      else
+        n = 3*n + 1
+      end
+      chain += 1
+    end
+    if chain > longest_chain
+      longest_chain = chain
+      longest_starting = starting_num
+    end
+    starting_num += 1
+  end
+  return longest_starting
+end
+
+puts collatz
 
 
